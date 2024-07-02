ac8525f1194754391e9dc48f1160b9d930a84c3f
Giuseppe Campanella <giuseppe.campanella@techedgegroup.com>
2024-07-02 22:49:25 +0200

    v. 1.1

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hello World! v. 1.1\n');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
---
+++ test/appTest.js
@@ -0,0 +1,8 @@
+import { assert } from 'chai';
+
+describe('simple test', function () {
+
+    it('math problem: 2+2=?', function () {
+        assert.isTrue(2 + 2 === 4, 'you are bad at math!');
+    });
+});
